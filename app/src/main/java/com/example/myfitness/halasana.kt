aeb866b26871353732dd6662edafc717e1a5d7b6
lucifer10 <akshatmudgal2003@gmail.com>
2022-05-30 00:32:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myfitness/halasana.kt
@@ -0,0 +1,38 @@
+package com.example.myfitness
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.service.autofill.OnClickAction
+import android.view.View
+import android.widget.ImageButton
+import android.content.Intent
+
+class halasana : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_halasana)
+        val halbutton:ImageButton = findViewById(R.id.halbutton)
+        halbutton.setOnClickListener {
+            val intent = Intent(this@halasana, halasana::class.java)
+            startActivity(intent)
+            finish();
+        }
+        val bhujangbutton:ImageButton = findViewById(R.id.bhujangbutton)
+        bhujangbutton.setOnClickListener {
+            val intent = Intent(this@halasana, bhujangasana::class.java)
+            startActivity(intent)
+            finish();
+        }
+        val dhanurbutton:ImageButton = findViewById(R.id.dhanurbutton)
+        dhanurbutton.setOnClickListener {
+            val intent = Intent(this@halasana, dhanurasana::class.java)
+            startActivity(intent)
+            finish();}
+        val sirbutton:ImageButton = findViewById(R.id.sirbutton)
+        sirbutton.setOnClickListener {
+            val intent = Intent(this@halasana, MainActivity::class.java)
+            startActivity(intent)
+            finish();}
+
+    }
+}
